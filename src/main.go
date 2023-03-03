f8dab8d225d3ac0718e42e69fe99907b93822cba
matt <controlgame@icloud.com>
2023-03-03 15:45:16 -0300

    added background effect

--- src/main.go
+++ src/main.go
@@ -13,27 +13,49 @@ type tubes struct {
 	active bool
 }
 type game struct {
-	pause      bool
-	over       bool
-	score      int
-	background rl.Texture2D
-	foreground rl.Texture2D
-	midground  rl.Texture2D
-	fxJump     rl.Sound
-	fxOver     rl.Music
-	player     goppy
-	highScore  int
+	pause         bool
+	over          bool
+	score         int
+	background    rl.Texture2D
+	foreground    rl.Texture2D
+	midground     rl.Texture2D
+	fxJump        rl.Sound
+	fxOver        rl.Music
+	player        goppy
+	scrollingBack float32
+	scrollingMid  float32
+	scrollingFore float32
+	highScore     int
 }
 
 func update(game *game) {
+	game.scrollingBack -= 0.1
+	game.scrollingMid -= 0.5
+	game.scrollingFore -= 1
+
+	if game.scrollingBack <= float32(game.background.Width)*2 {
+		game.scrollingBack = 0
+	}
+	if game.scrollingMid <= -float32(game.midground.Width)*2 {
+		game.scrollingMid = 0
+	}
+	if game.scrollingFore <= -float32(game.foreground.Width)*2 {
+		game.scrollingFore = 0
+	}
 
 }
 func draw(game *game) {
 	rl.BeginDrawing()
 
-	rl.ClearBackground(rl.RayWhite)
-	rl.DrawTexture(game.background, 0, 0, rl.Green)
+	rl.ClearBackground(rl.GetColor(0x052c46ff))
+	rl.DrawTextureEx(game.background, rl.Vector2{X: game.scrollingBack, Y: 20}, 0, 2.0, rl.White)
+	rl.DrawTextureEx(game.background, rl.Vector2{X: float32(game.background.Width)*2 + game.scrollingBack, Y: 20}, 0, 2.0, rl.White)
 
+	rl.DrawTextureEx(game.midground, rl.Vector2{X: game.scrollingMid, Y: 20}, 0, 2.0, rl.White)
+	rl.DrawTextureEx(game.midground, rl.Vector2{X: float32(game.midground.Width)*2 + game.scrollingMid, Y: 20}, 0, 2.0, rl.White)
+
+	rl.DrawTextureEx(game.foreground, rl.Vector2{X: game.scrollingFore, Y: 70}, 0, 2.0, rl.White)
+	rl.DrawTextureEx(game.foreground, rl.Vector2{X: float32(game.foreground.Width)*2 + game.scrollingFore, Y: 70}, 0, 2.0, rl.White)
 	rl.EndDrawing()
 }
 func (game *game) loadGame() {
@@ -53,11 +75,10 @@ func (game *game) unloadGame() {
 
 }
 func (game *game) initGame() {
-	game.highScore = 0
+
 	game.over = false
 	game.pause = false
 	game.score = 0
-
 	game.player = goppy{rl.Vector2{X: 0, Y: 400}}
 }
 func main() {
