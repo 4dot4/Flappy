a5d8461a59158d86fb47c29b7682435ed92b8ef8
matt <controlgame@icloud.com>
2023-03-04 20:52:50 -0300

    fixed some things

--- src/main.go
+++ src/main.go
@@ -4,8 +4,21 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+type Screen int
+
+const (
+	Menu Screen = iota
+	Logo
+	Scale float32 = 3
+)
+
 type goppy struct {
-	Coords rl.Vector2
+	DestRec      rl.Rectangle
+	FrameRec     rl.Rectangle
+	Rotation     float32
+	CurrentFrame int
+
+	FrameSpeed int
 }
 type tube struct {
 	Texture rl.Texture2D
@@ -13,15 +26,17 @@ type tube struct {
 	Active  bool
 }
 type Game struct {
-	Pause       bool
-	Over        bool
-	Score       int
-	SpriteSheet rl.Texture2D
-	FxJump      rl.Sound
-	FxOver      rl.Music
-	Player      goppy
-	Tube        tube
-	HighScore   int
+	Pause         bool
+	Over          bool
+	Score         int
+	SpriteSheet   rl.Texture2D
+	FxJump        rl.Sound
+	FxOver        rl.Music
+	Player        goppy
+	Tube          tube
+	HighScore     int
+	CurrentScreen Screen
+	FrameCounter  int
 }
 
 func update(Game *Game) {
@@ -29,8 +44,14 @@ func update(Game *Game) {
 }
 func draw(Game *Game) {
 	rl.BeginDrawing()
-	rl.ClearBackground(rl.GetColor(0x052c46ff))
-	rl.DrawTextureEx(Game.SpriteSheet, rl.Vector2{X: 0, Y: 0}, 0, 3, rl.White)
+	rl.ClearBackground(rl.White)
+
+	rl.DrawTexturePro(Game.SpriteSheet,
+		rl.Rectangle{X: 0, Y: 0, Width: 143, Height: 255},         //RecSource
+		rl.Rectangle{X: 0, Y: 0, Width: 143 * 4, Height: 255 * 4}, //Destiny
+		rl.Vector2{X: 143 / 2, Y: 255 / 2}, 0, rl.White)           //Origin
+	rl.DrawTexturePro(Game.SpriteSheet, Game.Player.FrameRec, Game.Player.DestRec, rl.Vector2{X: Game.Player.FrameRec.Width / 2, Y: Game.Player.FrameRec.Height / 2}, 0, rl.White)
+
 	rl.EndDrawing()
 }
 func (Game *Game) loadGame() {
@@ -47,8 +68,21 @@ func (Game *Game) unloadGame() {
 
 }
 func (Game *Game) initGame() {
+	Game.Player = goppy{
+		FrameRec: rl.Rectangle{
+			X:      3,
+			Y:      490,
+			Width:  24,
+			Height: 20,
+		},
+	}
+	Game.Player.DestRec = rl.Rectangle{
+		X:      50,
+		Y:      50,
+		Width:  Game.Player.FrameRec.Width * Scale,
+		Height: Game.Player.FrameRec.Height * Scale,
+	}
 
-	Game.HighScore = 0
 	Game.Over = false
 	Game.Pause = false
 	Game.Score = 0
