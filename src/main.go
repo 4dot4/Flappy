7f6f0470e1b9f7d9bba5931c8995027925bee4a8
matt <controlgame@icloud.com>
2023-03-04 21:38:52 -0300

    fixed some things

--- src/main.go
+++ src/main.go
@@ -17,8 +17,8 @@ type goppy struct {
 	FrameRec     rl.Rectangle
 	Rotation     float32
 	CurrentFrame int
-
-	FrameSpeed int
+	FrameSpeed   int
+	FrameCounter int
 }
 type tube struct {
 	Texture rl.Texture2D
@@ -36,29 +36,42 @@ type Game struct {
 	Tube          tube
 	HighScore     int
 	CurrentScreen Screen
-	FrameCounter  int
 }
 
 func update(Game *Game) {
-
+	Game.Player.FrameCounter++
+	if Game.Player.FrameCounter >= 60/Game.Player.FrameSpeed {
+		Game.Player.FrameCounter = 0
+		Game.Player.CurrentFrame++
+		if Game.Player.CurrentFrame > 2 {
+			Game.Player.CurrentFrame = 0
+		}
+		Game.Player.FrameRec.X = float32(Game.Player.CurrentFrame) * Game.Player.FrameRec.Width
+	}
+	if rl.IsKeyPressed(rl.KeyD) {
+		Game.Player.FrameSpeed++
+	}
+	if rl.IsKeyPressed(rl.KeyA) {
+		Game.Player.FrameSpeed--
+	}
 }
 func draw(Game *Game) {
 	rl.BeginDrawing()
 	rl.ClearBackground(rl.White)
-
+	//background
 	rl.DrawTexturePro(Game.SpriteSheet,
 		rl.Rectangle{X: 0, Y: 0, Width: 143, Height: 255},         //RecSource
 		rl.Rectangle{X: 0, Y: 0, Width: 143 * 4, Height: 255 * 4}, //Destiny
 		rl.Vector2{X: 143 / 2, Y: 255 / 2}, 0, rl.White)           //Origin
+
+	//floppy
 	rl.DrawTexturePro(Game.SpriteSheet, Game.Player.FrameRec, Game.Player.DestRec, rl.Vector2{X: Game.Player.FrameRec.Width / 2, Y: Game.Player.FrameRec.Height / 2}, 0, rl.White)
 
 	rl.EndDrawing()
 }
 func (Game *Game) loadGame() {
 
-	Game.FxJump = rl.LoadSound("./sounds/jump.mp3")
 	Game.SpriteSheet = rl.LoadTexture("./assets/SpriteSheet.png")
-	Game.FxOver = rl.LoadMusicStream("./sounds/gameOver.mp3")
 
 }
 func (Game *Game) unloadGame() {
@@ -72,7 +85,7 @@ func (Game *Game) initGame() {
 		FrameRec: rl.Rectangle{
 			X:      3,
 			Y:      490,
-			Width:  24,
+			Width:  28,
 			Height: 20,
 		},
 	}
@@ -82,6 +95,7 @@ func (Game *Game) initGame() {
 		Width:  Game.Player.FrameRec.Width * Scale,
 		Height: Game.Player.FrameRec.Height * Scale,
 	}
+	Game.Player.FrameSpeed = 8
 
 	Game.Over = false
 	Game.Pause = false
