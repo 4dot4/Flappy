f27de79ee89ed8cdba61519463a72af7c2a9854f
matt <controlgame@icloud.com>
2023-03-05 16:42:55 -0300

    added better sound effects

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
@@ -12,9 +14,14 @@ const (
 	Scale float32 = 3
 )
 
+type Circle struct {
+	Origin rl.Vector2
+	Radios float32
+}
 type goppy struct {
 	DestRec      rl.Rectangle
 	FrameRec     rl.Rectangle
+	CircleCol    Circle
 	Rotation     float32
 	CurrentFrame int
 	FrameSpeed   int
@@ -48,12 +55,26 @@ func update(Game *Game) {
 		}
 		Game.Player.FrameRec.X = float32(Game.Player.CurrentFrame) * Game.Player.FrameRec.Width
 	}
+	Game.Player.Rotation += 1
 	if rl.IsKeyPressed(rl.KeyD) {
 		Game.Player.FrameSpeed++
 	}
 	if rl.IsKeyPressed(rl.KeyA) {
 		Game.Player.FrameSpeed--
 	}
+	if rl.IsKeyDown(rl.KeyDown) {
+		Game.Player.DestRec.Y += 5
+	}
+	if rl.IsKeyDown(rl.KeyUp) {
+		Game.Player.DestRec.Y -= 5
+	}
+	if rl.IsKeyDown(rl.KeyRight) {
+		Game.Player.DestRec.X += 5
+	}
+	if rl.IsKeyDown(rl.KeyLeft) {
+		Game.Player.DestRec.X -= 5
+	}
+
 }
 func draw(Game *Game) {
 	rl.BeginDrawing()
@@ -65,8 +86,10 @@ func draw(Game *Game) {
 		rl.Vector2{X: 143 / 2, Y: 255 / 2}, 0, rl.White)           //Origin
 
 	//floppy
-	rl.DrawTexturePro(Game.SpriteSheet, Game.Player.FrameRec, Game.Player.DestRec, rl.Vector2{X: Game.Player.FrameRec.Width / 2, Y: Game.Player.FrameRec.Height / 2}, 0, rl.White)
+	rl.DrawTexturePro(Game.SpriteSheet, Game.Player.FrameRec, Game.Player.DestRec, rl.Vector2{X: 0, Y: 0}, 0, rl.White)
 
+	rl.DrawRectangleLines(Game.Player.DestRec.ToInt32().X+9, Game.Player.DestRec.ToInt32().Y, Game.Player.DestRec.ToInt32().Width-31, Game.Player.DestRec.ToInt32().Height, rl.Red)
+	rl.DrawText(fmt.Sprintf("X : %2f\nY :%2f", Game.Player.DestRec.X, Game.Player.DestRec.Y), 10, 10, 20, rl.Red)
 	rl.EndDrawing()
 }
 func (Game *Game) loadGame() {
@@ -83,18 +106,19 @@ func (Game *Game) unloadGame() {
 func (Game *Game) initGame() {
 	Game.Player = goppy{
 		FrameRec: rl.Rectangle{
-			X:      3,
-			Y:      490,
+			X:      0,
+			Y:      491,
 			Width:  28,
-			Height: 20,
+			Height: 12,
 		},
 	}
 	Game.Player.DestRec = rl.Rectangle{
-		X:      50,
-		Y:      50,
+		X:      140,
+		Y:      360,
 		Width:  Game.Player.FrameRec.Width * Scale,
 		Height: Game.Player.FrameRec.Height * Scale,
 	}
+
 	Game.Player.FrameSpeed = 8
 
 	Game.Over = false
